e99f64d56c7113ff7848c4e3a626596587f5be81
ch4zzzzz <zxz2933@163.com>
2019-08-25 14:58:36 +0800

    fix LazyLoader

--- src/directive/lazy-load.js
+++ src/directive/lazy-load.js
@@ -1,16 +1,15 @@
 import LazyLoader from '@/util/post-img-lazy-loader'
-let lazyLoader;
 
 const lazyLoadDirective = {
   bind (el, binding, vnode) {
-    lazyLoader = new LazyLoader(el);
+    vnode.$lazyLoader = new LazyLoader(el);
   },
   update (el, binding, vnode) {
     // lazyLoader ? 1 : lazyLoader = new LazyLoader(el);
   },
   unbind (el, binding, vnode) {
-    lazyLoader.destory();
-    lazyLoader = null;
+    vnode.$lazyLoader.destory();
+    vnode.$lazyLoader = null;
   }
 }
 
